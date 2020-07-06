f5fe0df36709cad42672ba4ff409dc2147155d5b
raja <46279750+rajaumer801@users.noreply.github.com>
2020-07-06 17:04:25 +0500

    disable debug

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ from flask import Flask, request
 from flask_mail import Mail, Message
 
 app = Flask(__name__)
-app.debug = True
 mail_settings = {
     "MAIL_SERVER": 'smtp.gmail.com',
     "MAIL_PORT": 465,
